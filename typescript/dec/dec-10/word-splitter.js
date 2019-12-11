b1aecda54231dc5b8881fa9bf436512a93067c55
lineville <lineville@vassar.edu>
2019-12-11 09:11:12 -0800

    dec 10 done
    Signed-off-by: lineville <lineville@vassar.edu>

--- typescript/dec/dec-10/word-splitter.js
+++ typescript/dec/dec-10/word-splitter.js
@@ -6,7 +6,26 @@
  * *  amount of words. If there's no way to break the text up, then return null.
  */
 var wordSplitter = function (input, limit) {
-    var result = null;
+    var allWords = input.split(" ");
+    if (allWords.filter(function (val) { return val.length > limit; }).length > 0) {
+        // * There exists a word greater than limit so it will not be possible
+        return null;
+    }
+    // * It is possible to split up the given input
+    var result = [];
+    var lineIndex = 0;
+    var line = "";
+    allWords.forEach(function (word) {
+        var lengthIfAdded = lineIndex + word.length + 1;
+        if (lengthIfAdded > limit) {
+            lineIndex = 0;
+            result.push(line);
+            line = "";
+        }
+        line += lineIndex === 0 ? word : " " + word;
+        lineIndex += word.length;
+    });
+    result.push(line);
     return result;
 };
 var arrayEquals = function (arr1, arr2) {
@@ -24,11 +43,17 @@ var testWordSplitter = function () {
     var input1 = "the quick brown fox jumps over the lazy dog";
     var limit1 = 10;
     var result1 = wordSplitter(input1, limit1);
-    var expectedResult1 = ["the quick", "brown fox", "jumps over", "the lazy", "dog"];
-    var expectedResult2 = ["the quick", "brown fox", "jumps over", "the lazy", "dog"];
+    var expectedResult1 = [
+        "the quick",
+        "brown fox",
+        "jumps over",
+        "the lazy",
+        "dog"
+    ];
     var passed = arrayEquals(result1, expectedResult1);
-    var mustBe = arrayEquals(expectedResult1, expectedResult2);
-    console.log(mustBe);
+    console.log(result1);
     console.log(passed ? "Passed" : "Failed");
+    var result2 = wordSplitter(input1, 2);
+    console.log(result2 === null ? "Passed" : "Failed");
 };
 testWordSplitter();
--- typescript/dec/dec-10/word-splitter.ts
+++ typescript/dec/dec-10/word-splitter.ts
@@ -8,35 +8,62 @@
  */
 
 const wordSplitter = (input: string, limit: number): Array<string> | null => {
-  let result: Array<string> | null = null;
-  // * Do the splitting logic
+  let allWords: Array<string> = input.split(" ");
+  if (allWords.filter((val: string) => val.length > limit).length > 0) {
+    // * There exists a word greater than limit so it will not be possible
+    return null;
+  }
+
+  // * It is possible to split up the given input
+  let result: Array<string> | null = [];
+  let lineIndex: number = 0;
+  let line: string = "";
+
+  allWords.forEach(function(word: string) {
+    let lengthIfAdded: number = lineIndex + word.length + 1;
+    if (lengthIfAdded > limit) {
+      lineIndex = 0;
+      result.push(line);
+      line = ""
+    }
+
+    line += lineIndex === 0 ? word : " " + word;
+    lineIndex += word.length;
+  });
+  result.push(line);
 
   return result;
 };
 
+const arrayEquals = (arr1: Array<string>, arr2: Array<string>): boolean => {
+  if (!arr1 || !arr2 || arr1.length !== arr2.length) return false;
 
-const arrayEquals = (arr1 : Array<string>, arr2 : Array<string>) : boolean => {
-    if (!arr1 || !arr2 || arr1.length !== arr2.length) return false;
-
-    let result : boolean = true;
-    arr1.forEach(function (str : string, idx: number) {
-        if (arr2[idx] !== str) {
-            result = false;
-        }
-    });
-    return result;
-}
+  let result: boolean = true;
+  arr1.forEach(function(str: string, idx: number) {
+    if (arr2[idx] !== str) {
+      result = false;
+    }
+  });
+  return result;
+};
 
 const testWordSplitter = (): void => {
   const input1: string = "the quick brown fox jumps over the lazy dog";
   const limit1: number = 10;
-  const result1 : Array<string> | null = wordSplitter(input1, limit1);
-  let expectedResult1 : Array<string> = ["the quick", "brown fox", "jumps over", "the lazy", "dog"]
-  let expectedResult2 : Array<string> = ["the quick", "brown fox", "jumps over", "the lazy", "dog"]
-  let passed : boolean = arrayEquals(result1, expectedResult1);
-  let mustBe : boolean = arrayEquals(expectedResult1, expectedResult2);
-  console.log(mustBe);
-  console.log(passed ? "Passed" : "Failed")
+  const result1: Array<string> | null = wordSplitter(input1, limit1);
+  let expectedResult1: Array<string> = [
+    "the quick",
+    "brown fox",
+    "jumps over",
+    "the lazy",
+    "dog"
+  ];
+  let passed: boolean = arrayEquals(result1, expectedResult1);
+  console.log(result1);
+  console.log(passed ? "Passed" : "Failed");
+
+  const result2 : Array<string> | null = wordSplitter(input1, 2);
+  console.log(result2 === null ? "Passed" : "Failed");
 };
 
-testWordSplitter()
+testWordSplitter();
