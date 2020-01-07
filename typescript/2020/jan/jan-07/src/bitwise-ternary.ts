30fcc1087017e28007774304805d642909b3e844
lineville <lineville@vassar.edu>
2020-01-07 13:59:36 -0800

    claened up

--- typescript/2020/jan/jan-07/src/bitwise-ternary.ts
+++ typescript/2020/jan/jan-07/src/bitwise-ternary.ts
@@ -1,6 +1,5 @@
 // * Daily Coding Problem January 07 2020
 
-
 // * [Medium] -- Facebook
 /**
  * * Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0.
@@ -8,29 +7,9 @@
  * * You can assume b can only be 1 or 0.
  */
 
-
 export const bitwiseTernary = (nums : Array<number>) : number => {
-    // * Array destructuring to declare three new 
     const [x, y, b] : Array<number> = nums
-    // * This is cheating below (or the way any sane human would write this)
-    // ! return b === 1 ? x : y
-    
-    
-    // * The way Facebook would like us to answer this
     const mask : number = -b
     return (x & mask) | (y & ~mask)
 }
-
-// const testBitWiseTernary = () : void => {
-//     const inputs1 : Array<number> = [10, 20, 1]
-//     const inputs2 : Array<number> = [10, 20, 0]
-//     const expected1 : number = 10;
-//     const expected2 : number = 20;
-//     const result1 : number = bitwiseTernary(inputs1);
-//     const result2 : number = bitwiseTernary(inputs2);
-//     didTestPass(expected1, result1);
-//     didTestPass(expected2, result2);
-// }
-
-// testBitWiseTernary()
 
