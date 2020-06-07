9361348eb26f3cd28c6be472c94df9d39d240464
liamneville <lin01@quantumscape.com>
2020-06-07 01:57:33 -0700

    restruct
    Signed-off-by: liamneville <lin01@quantumscape.com>

---
+++ dotnet/june/june-05/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace june_05
+{
+  class Program
+  {
+
+    /* Class Pair is used to return 
+		two values from getMinMax() */
+    public class Pair
+    {
+      public int min;
+      public int max;
+    }
+
+    static Pair getMinMax(int[] arr, int n)
+    {
+      Pair minmax = new Pair();
+      int i;
+
+      /* If array has even number of elements 
+			then initialize the first two elements 
+			as minimum and maximum */
+      if (n % 2 == 0)
+      {
+        if (arr[0] > arr[1])
+        {
+          minmax.max = arr[0];
+          minmax.min = arr[1];
+        }
+        else
+        {
+          minmax.min = arr[0];
+          minmax.max = arr[1];
+        }
+        i = 2;
+      }
+
+      /* set the startung index for loop */
+      /* If array has odd number of elements then 
+			initialize the first element as minimum and 
+			maximum */
+      else
+      {
+        minmax.min = arr[0];
+        minmax.max = arr[0];
+        i = 1;
+        /* set the startung index for loop */
+      }
+
+      /* In the while loop, pick elements in pair and 
+			compare the pair with max and min so far */
+      while (i < n - 1)
+      {
+        if (arr[i] > arr[i + 1])
+        {
+          if (arr[i] > minmax.max)
+          {
+            minmax.max = arr[i];
+          }
+          if (arr[i + 1] < minmax.min)
+          {
+            minmax.min = arr[i + 1];
+          }
+        }
+        else
+        {
+          if (arr[i + 1] > minmax.max)
+          {
+            minmax.max = arr[i + 1];
+          }
+          if (arr[i] < minmax.min)
+          {
+            minmax.min = arr[i];
+          }
+        }
+        i += 2;
+
+        /* Increment the index by 2 as two 
+				elements are processed in loop */
+      }
+      return minmax;
+    }
+
+    // Driver Code 
+    public static void Main(String[] args)
+    {
+      int[] arr = { 1000, 11, 445, 1, 330, 3000 };
+      int arr_size = 6;
+      Pair minmax = getMinMax(arr, arr_size);
+      Console.Write("Minimum element is {0}",
+        minmax.min);
+      Console.Write("\nMaximum element is {0}",
+        minmax.max);
+    }
+  }
+}
+
+// This code is contributed by 29AjayKumar
--- typescript/2020/jan/jan-09/tests/DirectionValidator.test.ts
+++ typescript/2020/jan/jan-09/tests/DirectionValidator.test.ts
@@ -1,173 +1,173 @@
-import { expect, assert } from 'chai'
+import { expect, assert } from "chai";
 import {
   validDirections,
   constraintSatisfied,
   invertDir,
   computeDirection,
   Direction,
-} from './../src/DirectionValidator'
+} from "./../src/DirectionValidator";
 
-describe('Direction Validator', () => {
-  it('validDirections : invalid', () => {
-    let result = validDirections(['A N B', 'B NE C', 'C N A'])
-    expect(result).to.be.false
-  })
+describe("Direction Validator", () => {
+  it("validDirections : invalid", () => {
+    let result = validDirections(["A N B", "B NE C", "C N A"]);
+    expect(result).to.be.false;
+  });
 
-  it('validDirections : invalid', () => {
-    let result = validDirections(['A N B', 'B NE C', 'C E A'])
-    expect(result).to.be.false
-  })
+  it("validDirections : invalid", () => {
+    let result = validDirections(["A N B", "B NE C", "C E A"]);
+    expect(result).to.be.false;
+  });
 
-  it('validDirections : valid', () => {
-    let result = validDirections(['A NW B', 'A N B'])
-    expect(result).to.be.true
-  })
+  it("validDirections : valid", () => {
+    let result = validDirections(["A NW B", "A N B"]);
+    expect(result).to.be.true;
+  });
 
-  it('validDirections : valid', () => {
-    let result = validDirections(['A NW B', 'B N C', 'D E C'])
-    expect(result).to.be.true
-  })
-})
+  it("validDirections : valid", () => {
+    let result = validDirections(["A NW B", "B N C", "D E C"]);
+    expect(result).to.be.true;
+  });
+});
 
-describe('Constraint Checker', () => {
-  it('constraintSatisfied : N', () => {
-    let result = constraintSatisfied('A N B', {
+describe("Constraint Checker", () => {
+  it("constraintSatisfied : N", () => {
+    let result = constraintSatisfied("A N B", {
       A: { x: 0, y: 0 },
       B: { x: 0, y: -1 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied: S', () => {
-    let result = constraintSatisfied('A S B', {
+  it("constraintSatisfied: S", () => {
+    let result = constraintSatisfied("A S B", {
       A: { x: 0, y: 0 },
       B: { x: 0, y: 1 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied: W', () => {
-    let result = constraintSatisfied('A W B', {
+  it("constraintSatisfied: W", () => {
+    let result = constraintSatisfied("A W B", {
       A: { x: 0, y: 0 },
       B: { x: 1, y: 0 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied : E', () => {
-    let result = constraintSatisfied('A E B', {
+  it("constraintSatisfied : E", () => {
+    let result = constraintSatisfied("A E B", {
       A: { x: 1, y: 0 },
       B: { x: 0, y: 0 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied : NW', () => {
-    let result = constraintSatisfied('A NW B', {
+  it("constraintSatisfied : NW", () => {
+    let result = constraintSatisfied("A NW B", {
       A: { x: 0, y: 0 },
       B: { x: 1, y: -1 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied : NE', () => {
-    let result = constraintSatisfied('A NE B', {
+  it("constraintSatisfied : NE", () => {
+    let result = constraintSatisfied("A NE B", {
       A: { x: 0, y: 0 },
       B: { x: -1, y: -1 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied : SW', () => {
-    let result = constraintSatisfied('A SW B', {
+  it("constraintSatisfied : SW", () => {
+    let result = constraintSatisfied("A SW B", {
       A: { x: 0, y: 0 },
       B: { x: 1, y: 1 },
-    })
-    expect(result).to.be.true
-  })
+    });
+    expect(result).to.be.true;
+  });
 
-  it('constraintSatisfied : SE', () => {
-    let result = constraintSatisfied('A SE B', {
+  it("constraintSatisfied : SE", () => {
+    let result = constraintSatisfied("A SE B", {
       A: { x: 0, y: 0 },
       B: { x: -1, y: 1 },
-    })
-    expect(result).to.be.true
-  })
-})
+    });
+    expect(result).to.be.true;
+  });
+});
 
-describe('Invert Direction', () => {
-  it('Invert Dir : N', () => {
-    let result = invertDir('N')
-    expect(result).to.equal('S')
-  })
+describe("Invert Direction", () => {
+  it("Invert Dir : N", () => {
+    let result = invertDir("N");
+    expect(result).to.equal("S");
+  });
 
-  it('Invert Dir : S', () => {
-    let result = invertDir('S')
-    expect(result).to.equal('N')
-  })
+  it("Invert Dir : S", () => {
+    let result = invertDir("S");
+    expect(result).to.equal("N");
+  });
 
-  it('Invert Dir : E', () => {
-    let result = invertDir('E')
-    expect(result).to.equal('W')
-  })
+  it("Invert Dir : E", () => {
+    let result = invertDir("E");
+    expect(result).to.equal("W");
+  });
 
-  it('Invert Dir : W', () => {
-    let result = invertDir('W')
-    expect(result).to.equal('E')
-  })
+  it("Invert Dir : W", () => {
+    let result = invertDir("W");
+    expect(result).to.equal("E");
+  });
 
-  it('Invert Dir : NW', () => {
-    let result = invertDir('NW')
-    expect(result).to.equal('SE')
-  })
+  it("Invert Dir : NW", () => {
+    let result = invertDir("NW");
+    expect(result).to.equal("SE");
+  });
 
-  it('Invert Dir : NE', () => {
-    let result = invertDir('NE')
-    expect(result).to.equal('SW')
-  })
+  it("Invert Dir : NE", () => {
+    let result = invertDir("NE");
+    expect(result).to.equal("SW");
+  });
 
-  it('Invert Dir : SW', () => {
-    let result = invertDir('SW')
-    expect(result).to.equal('NE')
-  })
+  it("Invert Dir : SW", () => {
+    let result = invertDir("SW");
+    expect(result).to.equal("NE");
+  });
 
-  it('Invert Dir : SE', () => {
-    let result = invertDir('SE')
-    expect(result).to.equal('NW')
-  })
-})
+  it("Invert Dir : SE", () => {
+    let result = invertDir("SE");
+    expect(result).to.equal("NW");
+  });
+});
 
-describe('Compute Direction', () => {
-  it('Direction: N', () => {
-    let result = computeDirection('N', { x: 0, y: 0 })
-    expect(result).to.eql({ x: 0, y: -1 })
-  })
-  it('Direction: S', () => {
-    let result = computeDirection('S', { x: 0, y: 0 })
-    expect(result).to.eql({ x: 0, y: 1 })
-  })
-  it('Direction: E', () => {
-    let result = computeDirection('E', { x: 0, y: 0 })
-    expect(result).to.eql({ x: -1, y: 0 })
-  })
-  it('Direction: W', () => {
-    let result = computeDirection('W', { x: 0, y: 0 })
-    expect(result).to.eql({ x: 1, y: 0 })
-  })
-  it('Direction: NW', () => {
-    let result = computeDirection('NW', { x: 0, y: 0 })
-    expect(result).to.eql({ x: 1, y: -1 })
-  })
-  it('Direction: NE', () => {
-    let result = computeDirection('NE', { x: 0, y: 0 })
-    expect(result).to.eql({ x: -1, y: -1 })
-  })
-  it('Direction: SW', () => {
-    let result = computeDirection('SW', { x: 0, y: 0 })
-    expect(result).to.eql({ x: 1, y: 1 })
-  })
-  it('Direction: SE', () => {
-    let result = computeDirection('SE', { x: 0, y: 0 })
-    expect(result).to.eql({ x: -1, y: 1 })
-  })
-})
+describe("Compute Direction", () => {
+  it("Direction: N", () => {
+    let result = computeDirection("N", { x: 0, y: 0 });
+    expect(result).to.eql({ x: 0, y: -1 });
+  });
+  it("Direction: S", () => {
+    let result = computeDirection("S", { x: 0, y: 0 });
+    expect(result).to.eql({ x: 0, y: 1 });
+  });
+  it("Direction: E", () => {
+    let result = computeDirection("E", { x: 0, y: 0 });
+    expect(result).to.eql({ x: -1, y: 0 });
+  });
+  it("Direction: W", () => {
+    let result = computeDirection("W", { x: 0, y: 0 });
+    expect(result).to.eql({ x: 1, y: 0 });
+  });
+  it("Direction: NW", () => {
+    let result = computeDirection("NW", { x: 0, y: 0 });
+    expect(result).to.eql({ x: 1, y: -1 });
+  });
+  it("Direction: NE", () => {
+    let result = computeDirection("NE", { x: 0, y: 0 });
+    expect(result).to.eql({ x: -1, y: -1 });
+  });
+  it("Direction: SW", () => {
+    let result = computeDirection("SW", { x: 0, y: 0 });
+    expect(result).to.eql({ x: 1, y: 1 });
+  });
+  it("Direction: SE", () => {
+    let result = computeDirection("SE", { x: 0, y: 0 });
+    expect(result).to.eql({ x: -1, y: 1 });
+  });
+});
