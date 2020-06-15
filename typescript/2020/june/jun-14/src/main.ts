4014b36ad703bcac0ca8bce09dc3be47ae6d5f88
Liam Neville <lneville@quantumscape.com>
2020-06-15 11:17:18 -0700

    incomplete jun 14

---
+++ typescript/2020/june/jun-14/src/main.ts
@@ -0,0 +1,17 @@
+// * Daily Coding Problem June 14th 2020
+
+// * [Easy] -- Square
+
+// * The Sieve of Eratosthenes is an algorithm used to generate all prime numbers smaller
+// * than N. The method is to take increasingly larger prime numbers, and mark their
+// * multiples as composite.
+
+// ! Ex: to find all primes less than 100, we would first mark [4, 6, 8, ...]
+// ! (multiples of two), then [6, 9, 12, ...] (multiples of three), and so on.
+// ! Once we have done this for all primes less than N, the unmarked numbers that remain will be prime.
+
+// * Implement this algorithm.
+
+// * Bonus: Create a generator that produces primes indefinitely (that is, without taking N as an input).
+
+export const simpleFunction = (): string => 'Hello World!'
