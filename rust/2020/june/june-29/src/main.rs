ca780723e1a2396f17f05eed77e10d7591a6463d
liamneville <lin01@quantumscape.com>
2020-07-01 10:42:37 -0700

    remove extras
    Signed-off-by: liamneville <lin01@quantumscape.com>

---
+++ rust/2020/june/june-29/src/main.rs
@@ -0,0 +1,15 @@
+// * Daily Coding Problem June 29 2020
+
+// * [Easy] -- Morgan Stanley
+
+// * In Ancient Greece, it was common to write text with the first line going left to right,
+// * the second line going right to left, and continuing to go back and forth. This style was called "boustrophedon".
+
+// ! Given a binary tree, write an algorithm to print the nodes in boustrophedon order.
+
+// use binary_tree::BinaryTree;
+
+fn main() {
+    println!("Binary Tree in boustrophedon order");
+    // let tree = BinaryTree {};
+}
