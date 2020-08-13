fc54ad5d81bfbb248e3ba8d0dd653a09e0812888
Liam Neville <lneville@quantumscape.com>
2020-08-13 14:59:12 -0700

    small tweak

--- python/2020/august/aug-13/main.py
+++ python/2020/august/aug-13/main.py
@@ -2,17 +2,19 @@ from clock import closestDegrees
 
 # * ____________________________MAIN_________________________________
 
-# * Basic wrapper for closestDegrees that allows user to input a time
-# * and view the smallest degrees and continue indefinitely
+# * Prints out the smallest angle in degrees that seperates the minutes
+# * and hours hands of a clock at the time specified by the user.
 
 
 def main():
-    keepGoing = True
-    while keepGoing:
+    shouldKeepGoing = True
+    while shouldKeepGoing:
         time = input("Please enter a time in the format: hh:mm --> ")
-        print(
-            f"Smallest angle between hours and minutes hands at {time} is {closestDegrees(time)}")
-        keepGoing = input("Again? [Y/N]").capitalize() == "Y"
+        print(f"Angle at {time} is {closestDegrees(time)} °")
+        userResponse = input("Another? ( Y / N )").capitalize()
+        shouldKeepGoing = userResponse == "Y"
+    print("Have a good one 😁!")
+    exit(0)
 
 
 # ! This is where the program starts!
