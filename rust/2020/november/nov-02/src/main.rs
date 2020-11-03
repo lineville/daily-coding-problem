c44df112fdf5640cb15ee2a8c8021e49f6149c9c
liamneville <lineville@LT-19ZX0X2.quantumscape.com>
2020-11-03 09:43:20 -0800

    continued nov-02

--- rust/2020/november/nov-02/src/main.rs
+++ rust/2020/november/nov-02/src/main.rs
@@ -7,16 +7,19 @@
 
 // * If two bold tags overlap or are contiguous, they should be merged.
 
-fn embolden(s: &str, list: Vec<String>) -> String {
+fn embolden(s: &str, list: Vec<&str>) -> String {
     let mut result = String::from(s);
-
+    let starting_indexes: Vec<usize> = list.iter().map(|sub| s.find(sub).unwrap()).collect();
+    for (_i, &idx) in starting_indexes.iter().enumerate() {
+        result.insert_str(idx, "<b>");
+    }
     return result;
 }
 
 fn main() {
     println!("Embolden text!\n\n------------------");
     let text = "abcdefg";
-    let list = vec![String::from("bc"), String::from("ef")];
+    let list = vec!["bc", "ef"];
     let result = embolden(text, list);
     println!("{} --> {}", text, result);
 }
@@ -26,13 +29,13 @@ fn main() {
 #[test]
 fn test_embolden_1() {
     let text = "abcdefg";
-    let list = vec![String::from("bc"), String::from("ef")];
+    let list = vec!["bc", "ef"];
     assert_eq!(embolden(text, list), String::from("a<b>bc</b>d<b>ef</b>g"));
 }
 
 #[test]
 fn test_embolden_2() {
     let text = "abcdefg";
-    let list = vec![String::from("bcd"), String::from("def")];
+    let list = vec!["bcd", "def"];
     assert_eq!(embolden(text, list), String::from("a<d>bcdef</b>g"));
 }
