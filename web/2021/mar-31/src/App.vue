9d526551f5f103426242cce0e73148d26cfc329e
lineville <lneville@quantumscape.com>
2021-03-31 22:26:57 -0700

    added vue chessboard

---
+++ web/2021/mar-31/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ChessBoard />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ChessBoard from './components/ChessBoard.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ChessBoard,
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web/2021/mar-31/src/components/ChessBoard.vue
@@ -0,0 +1,67 @@
+<template>
+  <h3>Chess Board</h3>
+
+  <ul>
+    <li v-for="row in board" v-bind:key="row">
+      <span v-for="cell in row" v-bind:key="cell">{{ cell }}</span>
+    </li>
+  </ul>
+
+  <p>Attacking Pairs: {{ attackingPairs }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'ChessBoard',
+  data() {
+    return {
+      size: 5,
+      bishops: [
+        { id: 1, x: 0, y: 0 },
+        { id: 2, x: 1, y: 2 },
+        { id: 3, x: 2, y: 2 },
+        { id: 4, x: 4, y: 0 },
+        { id: 5, x: 3, y: 1 },
+      ],
+    }
+  },
+  computed: {
+    board(): Array<Array<string>> {
+      let board = new Array(this.size)
+      for (let i = 0; i < this.size; i++) {
+        board[i] = new Array(this.size)
+        for (let j = 0; j < this.size; j++) {
+          const isBishop = this.bishops.filter((b) => b.x === i && b.y === j)
+            .length
+          board[i][j] = isBishop ? '♝' : '_'
+        }
+      }
+      return board
+    },
+    attackingPairs(): Array<[number, number]> {
+      let pairs = new Array<[number, number]>()
+      this.bishops.forEach((b, i) => {
+        for (let j = i + 1; j < this.bishops.length; j++) {
+          const deltaX = Math.abs(b.x - this.bishops[j].x)
+          const deltaY = Math.abs(b.y - this.bishops[j].y)
+          if (deltaX === deltaY) {
+            pairs.push([b.id, this.bishops[j].id])
+          }
+        }
+      })
+      return pairs
+    },
+  },
+})
+</script>
+
+<style scoped>
+span {
+  margin: 0px 2px 0px 2px;
+}
+
+ul {
+  list-style: none;
+}
+</style>
