e173aa275d7b448b3f4d6f703a8e2b3477601d93
lineville <lin01@quantumscape.com>
2021-05-08 11:16:32 -0700

    fixes
    Signed-off-by: lineville <lin01@quantumscape.com>

--- rust/2021/may/may-02/src/main.rs
+++ rust/2021/may/may-02/src/main.rs
@@ -5,6 +5,7 @@
 // Given an array of integers, return a new array such that each element at index i of the new
 // array is the product of all the numbers in the original array except the one at i.
 
+// FOLLOW UP
 // * Don't user division! (If we could this would be trivial...)
 
 // fn trivial_solution(nums: &[i32]) -> Vec<i32> {
@@ -14,18 +15,18 @@
 
 fn products_excluding_me(nums: &[i32]) -> Vec<i32> {
     let mut result = vec![1; nums.len()];
-    let mut multiplier = 1;
+    let mut temp = 1;
 
     for i in 0..nums.len() {
-        result[i] = multiplier;
-        multiplier *= nums[i];
+        result[i] = temp;
+        temp *= nums[i];
     }
 
-    multiplier = 1;
+    temp = 1;
 
     for i in (0..nums.len()).rev() {
-        result[i] *= multiplier;
-        multiplier *= nums[i];
+        result[i] *= temp;
+        temp *= nums[i];
     }
 
     return result;
@@ -47,4 +48,11 @@ fn test_product_excluding_me_1() {
 fn test_product_excluding_me_2() {
     let nums = vec![3, 2, 1];
     assert_eq!(products_excluding_me(&nums), vec![2, 3, 6]);
+}
+
+
+#[test]
+fn test_product_excluding_me_3() {
+    let nums = vec![5, 7, 11, 15, 2, 9, 12];
+    assert_eq!(products_excluding_me(&nums), vec![249480, 178200, 113400, 83160, 623700, 138600, 103950]);
 }
