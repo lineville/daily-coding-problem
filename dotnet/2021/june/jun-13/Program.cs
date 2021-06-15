7580ddfebfcbdf0d3c8dae6197d48977c899a6dd
lineville <lin01@quantumscape.com>
2021-06-14 17:25:45 -0700

    dotnet soundex mapping

---
+++ dotnet/2021/june/jun-13/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace jun_13
+{
+    class Program
+    {
+
+        private static string SimulateDominoes(string initialDominoes)
+        {
+            char[] result = new char[initialDominoes.Count()];
+            char[] doms = initialDominoes.ToCharArray();
+            bool anyChanges = true;
+
+            while (anyChanges)
+            {
+                anyChanges = false;
+            
+                for (int i = 0; i < doms.Length; i++)
+                {
+                    char d = doms[i];
+
+                    if (d == '.')
+                    {
+                        if (i != 0 && i != doms.Length - 1 && doms[i - 1] == 'R' && doms[i + 1] == 'L')
+                        {
+                            result[i] = d;
+                        } else if (i != 0 && doms[i - 1] == 'R')
+                        {
+                            result[i] = 'R';
+                            anyChanges = true;
+                        } else if (i != doms.Length - 1 && doms[i + 1] == 'L')
+                        {
+                            result[i] = 'L';
+                            anyChanges = true;
+                        } else {
+                            result[i] = d;
+                        }
+                    } else {
+                        result[i] = d;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string input1 = ".L.R....L";
+            string input2 = "..R...L.L";
+
+            Console.WriteLine($"{input1} --> {SimulateDominoes(input1)}");
+            Console.WriteLine($"{input2} --> {SimulateDominoes(input2)}");
+        }
+    }
+}
---
+++ dotnet/2021/june/jun-14/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Soundex is an algorithm used to categorize phonetically, such that two names that sound 
+// alike but are spelled differently have the same representation.
+
+// Soundex maps every name to a string consisting of one letter and three numbers, like M460.
+
+// One version of the algorithm is as follows:
+
+// 1. Remove consecutive consonants with the same sound (for example, change ck -> c).
+// 2. Keep the first letter. The remaining steps only apply to the rest of the string.
+// 3. Remove all vowels, including y, w, and h.
+// 4. Replace all consonants with the following digits:
+//          b, f, p, v → 1
+//          c, g, j, k, q, s, x, z → 2
+//          d, t → 3
+//          l → 4
+//          m, n → 5
+//          r → 6
+// 5. If you don't have three numbers yet, append zeros until you do. Keep the first three numbers.
+
+namespace jun_14
+{
+    class Program
+    {
+
+        private static IDictionary<string, char> consonantMapping = new Dictionary<string, char>()
+        {
+            { "ck", 'c'}
+            // TODO add more mappings of 2 letter pairs that are sonically equivalent to a single letter
+        };
+
+        private static HashSet<char> LETTERS_TO_EXCLUDE = new HashSet<char>()
+        {
+            'a', 'e', 'i', 'o', 'u', 'y', 'w', 'h'
+        };
+
+        private static IDictionary<char, int> letterMapping = new Dictionary<char, int>()
+        {
+            { 'b', 1 },
+            { 'f', 1 },
+            { 'p', 1 },
+            { 'v', 1 },
+            { 'c', 2 },
+            { 'g', 2 },
+            { 'j', 2 },
+            { 'k', 2 },
+            { 'q', 2 },
+            { 's', 2 },
+            { 'x', 2 },
+            { 'z', 2 },
+            { 'd', 3 },
+            { 't', 3 },
+            { 'l', 4 },
+            { 'm', 5 },
+            { 'n', 5 },
+            { 'r', 6 }
+        };
+
+        private static string SoundexMapping(string name)
+        {
+            char firstLetter = name[0];
+            
+            List<int> numericalCode = new List<int>();
+
+            List<char> condensedWord = name.ToLower().Substring(1).ToList();
+
+            // Stop one iteration early since we are scanning pairs at a time using i + 1
+            for (var i = 0; i < condensedWord.Count - 1; i++)
+            {
+                // Scan two letters at a time checking the mapping of pairs to single letters
+                string nextTwo = "" + condensedWord[i] + condensedWord[i + 1];
+
+                // Replace any pairs of 2 alike sounding with the single char
+                if (consonantMapping.ContainsKey(nextTwo))
+                {
+                    // Remove the first of the two letters
+                    condensedWord.RemoveAt(i);
+
+                    // Replace the second letter with the new one
+                    condensedWord[i] = consonantMapping[nextTwo];
+                }
+            }
+
+            // Apply rules 3 and 4 to get the 3 digit numerical code
+            foreach (char c in condensedWord)
+            {
+                bool invalidLetter = LETTERS_TO_EXCLUDE.Contains(c);
+                bool alreadyUsedNumber = letterMapping.ContainsKey(c) && numericalCode.Contains(letterMapping[c]);
+                
+                if (invalidLetter || alreadyUsedNumber)
+                {
+                    continue;
+                }
+
+                numericalCode.Add(letterMapping[c]);
+            }
+
+
+            // Pad out with zeroes
+            while (numericalCode.Count < 3)
+            {
+                numericalCode.Add(0);
+            }
+
+            return firstLetter + string.Join("", numericalCode.ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            string name1 = "Jackson";
+            string name2 = "Jaxen";
+            
+            Console.WriteLine($"{name1} --> {SoundexMapping(name1)}");
+            Console.WriteLine($"{name2} --> {SoundexMapping(name2)}");
+        }
+    }
+}
