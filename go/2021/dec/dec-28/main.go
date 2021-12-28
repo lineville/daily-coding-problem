9088616669c5f183457c9238128f0d47b85097d9
lineville <liamgneville@gmail.com>
2021-12-28 12:49:20 -0800

    added test
    Signed-off-by: lineville <liamgneville@gmail.com>

--- go/2021/dec/dec-28/main.go
+++ go/2021/dec/dec-28/main.go
@@ -4,7 +4,9 @@
 
 // Implement a PrefixMapSum class with the following methods:
 
-// insert(key: str, value: int): Set a given key's value in the map. If the key already exists, overwrite the value.
+// insert(key: str, value: int): Set a given key's value in the map.
+// If the key already exists, overwrite the value.
+
 // sum(prefix: str): Return the sum of all values of keys that begin with a given prefix.
 
 package main
@@ -18,10 +20,12 @@ type PrefixMapSum struct {
 	m map[string]int
 }
 
+// O(1) time complexity insert/override
 func (p *PrefixMapSum) insert(key string, value int) {
 	p.m[key] = value
 }
 
+// O(N) where N is the number of keys in the map for sum
 func (p *PrefixMapSum) sum(prefix string) int {
 	sum := 0
 	for k, v := range p.m {
@@ -34,7 +38,13 @@ func (p *PrefixMapSum) sum(prefix string) int {
 
 func main() {
 	prefixMapSum := PrefixMapSum{m: make(map[string]int)}
+
 	prefixMapSum.insert("columnar", 3)
+	fmt.Println("Adding columnar with value 3")
+
 	prefixMapSum.insert("column", 2)
-	fmt.Printf("%d\n", prefixMapSum.sum("col"))
+	fmt.Println("Adding column with value 2")
+
+	prefix := "column"
+	fmt.Printf("Sum of %s: %d\n", prefix, prefixMapSum.sum(prefix)) // 5
 }
--- go/2021/dec/dec-28/main_test.go
+++ go/2021/dec/dec-28/main_test.go
@@ -11,3 +11,13 @@ func TestPrefixMapSum(t *testing.T) {
 		t.Errorf("Expected 5, got %d", result)
 	}
 }
+
+func TestPrefixMapSum2(t *testing.T) {
+	prefixMapSum := PrefixMapSum{m: make(map[string]int)}
+	prefixMapSum.insert("columnar", 3)
+	prefixMapSum.insert("column", 2)
+	result := prefixMapSum.sum("columna")
+	if result != 3 {
+		t.Errorf("Expected 3, got %d", result)
+	}
+}
