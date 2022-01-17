1e75eab8125d9f124c2a910da4291e9c3a490880
Liam Neville <lineville@Liams-MacBook-Pro-2.local>
2022-01-17 11:06:11 -0800

    sieve of erasthones
    Signed-off-by: Liam Neville <lineville@github.com>

---
+++ dotnet/2022/jan/jan-17/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// * Daily Coding Problem Jan 17 2022
+
+// * [Easy] -- Square
+
+// The Sieve of Eratosthenes is an algorithm used to generate all prime numbers smaller than N.
+//  The method is to take increasingly larger prime numbers, and mark their multiples as composite.
+
+// For example, to find all primes less than 100, we would first mark [4, 6, 8, ...] (multiples of two),
+//  then [6, 9, 12, ...] (multiples of three), and so on. Once we have done this for all primes less 
+// than N, the unmarked numbers that remain will be prime.
+
+// ! Implement the sieve of eratosthenes
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] primes = SieveOfEratosthenes(100);
+        foreach (int prime in primes)
+        {
+            Console.WriteLine(prime);
+        }
+    }
+
+    public static int[] SieveOfEratosthenes(int max)
+    {
+        bool[] isComposite = new bool[max + 1];
+        for (int i = 2; i <= max; i++)
+        {
+            if (!isComposite[i])
+            {
+                for (int j = i * i; j <= max; j += i)
+                {
+                    isComposite[j] = true;
+                }
+            }
+        }
+
+        List<int> primes = new List<int>();
+        for (int i = 2; i <= max; i++)
+        {
+            if (!isComposite[i])
+            {
+                primes.Add(i);
+            }
+        }
+
+        return primes.ToArray();
+    }
+}
